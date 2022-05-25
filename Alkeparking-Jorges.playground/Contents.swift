15a30e660776376a08dba477c02303e2a3702b01
Jorge Restrepo <jorge.restrepo@mercadolibre.com.co>
2022-05-25 10:03:34 -0500

    Respuesta pregunta 1.

--- Alkeparking-Jorges.playground/Contents.swift
+++ Alkeparking-Jorges.playground/Contents.swift
@@ -5,6 +5,12 @@ protocol Parkable {
 }
 
 struct Parking {
+//    ¿ Por qué se define vehicles como un Set?
+//    Porque un Set es más ligero que un arreglo ya que no tiene
+//    en cuenta el orden de los elementos ni permite duplicados.
+//    En este caso, además, resulta conveniente porque ayuda a
+//    garantizar que nunca tendremos vehículos duplicados en nuestro
+//    parqueadero.
     var vehicles: Set<Vehicle> = []
 }
 
