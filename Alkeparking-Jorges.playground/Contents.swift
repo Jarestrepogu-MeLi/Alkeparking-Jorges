746f36a8ac27ac0a177da7feaa77e68be1ae44a1
jobernal <jorge.bernal@mercadolibre.com.co>
2022-05-25 16:57:21 -0500

    Ejercicio 7 estable

--- Alkeparking-Jorges.playground/Contents.swift
+++ Alkeparking-Jorges.playground/Contents.swift
@@ -42,19 +42,66 @@ struct Parking {
      garantizar que nunca tendremos vehículos duplicados en nuestro
      parqueadero. */
     var vehicles: Set<Vehicle> = []
+    
+    let maxSlots = 20
+    
+    mutating func checkInVehicle(_ vehicle: Vehicle, onFinish:
+                                 (Bool) -> Void) {
+        
+        guard vehicles.count <= maxSlots - 1 else {
+            onFinish(false)
+            return
+        }
+
+        for v in vehicles {
+            if vehicle.plate == v.plate {
+                onFinish(false)
+                return
+            }
+        }
+        
+        onFinish(true)
+        vehicles.insert(vehicle)
+    }
+    
+    mutating func checkOutVehicle(plate: String, onSuccess:(Int) -> (), onError:(Error) -> ()) {
+    //La función tiene que ser mutating para poder modificar el struct.
+        
+        var currentVehicle: Vehicle
+        
+        for v in vehicles {
+            if v.plate == plate {
+                currentVehicle = v
+                onSuccess(currentVehicle.type.rate)
+                vehicles.remove(currentVehicle)
+                return
+            } else {
+                print("No lo hubo")
+            }
+        }
+        
+        print("Me salí")
+        
+        
+    }
+    
 }
 
+
+
 struct Vehicle: Parkable, Hashable {
+    
     let plate: String
     let type: VehicleType
     let checkInTime: Date
     var parkedTime: Int {
         Calendar.current.dateComponents([.second], from: checkInTime, to: Date()).second ?? 0
     }
+    let discountCard: String?
+    
     /* ¿Qué tipo de propiedad permite este comportamiento:
      lazy properties, computed properties o static properties?
      Se usa una propiedad computada ya que esta calcula el valor cuando es llamada*/
-    let discountCard: String?
     /* ¿Dónde deben agregarse las propiedades, en Parkable, Vehicle o en ambos?
      En ambos porque en el protocolo definimos el requisito y en la estructura satisfacemos el requisito
      
@@ -71,24 +118,59 @@ struct Vehicle: Parkable, Hashable {
         return lhs.plate == rhs.plate
     }
     
-    mutating func totalParkingTime() -> Int {
-        return 0
-    }
 }
 
 var alkeParking = Parking()
-let car = Vehicle(plate: "AA111AA", type: VehicleType.car, checkInTime: Date(), discountCard: "DISCOUNT_CARD_001")
-let moto = Vehicle(plate: "B222BBB", type: VehicleType.moto, checkInTime: Date(), discountCard: nil)
-let miniBus = Vehicle(plate: "CC333CC", type: VehicleType.miniBus, checkInTime: Date(), discountCard: nil)
-let bus = Vehicle(plate: "DD444DD", type: VehicleType.bus, checkInTime: Date(), discountCard: "DISCOUNT_CARD_002")
-alkeParking.vehicles.insert(car)
-alkeParking.vehicles.insert(moto)
-alkeParking.vehicles.insert(miniBus)
-alkeParking.vehicles.insert(bus)
-alkeParking.vehicles.forEach({ vehicle in
-    DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-        print(vehicle.parkedTime)
+let vehicle1 = Vehicle(plate: "AA111AA", type: VehicleType.car, checkInTime: Date(), discountCard: "DISCOUNT_CARD_001")
+let vehicle2 = Vehicle(plate: "B222BBB", type: VehicleType.moto, checkInTime: Date(), discountCard: nil)
+let vehicle3 = Vehicle(plate: "CC333CC", type: VehicleType.miniBus, checkInTime: Date(), discountCard: nil)
+let vehicle4 = Vehicle(plate: "DD444DD", type: VehicleType.bus, checkInTime: Date(), discountCard: "DISCOUNT_CARD_002")
+let vehicle5 = Vehicle(plate: "AA111BB", type: VehicleType.car, checkInTime: Date(), discountCard: "DISCOUNT_CARD_003")
+let vehicle6 = Vehicle(plate: "B222CCC", type: VehicleType.moto, checkInTime: Date(), discountCard: "DISCOUNT_CARD_004")
+let vehicle7 = Vehicle(plate: "CC333DD", type: VehicleType.miniBus, checkInTime: Date(), discountCard: nil)
+let vehicle8 = Vehicle(plate: "DD444EE", type: VehicleType.bus, checkInTime: Date(), discountCard: "DISCOUNT_CARD_005")
+let vehicle9 = Vehicle(plate: "AA111CC", type: VehicleType.car, checkInTime: Date(), discountCard: nil)
+let vehicle10 = Vehicle(plate: "B222DDD", type: VehicleType.moto, checkInTime: Date(), discountCard: nil)
+let vehicle11 = Vehicle(plate: "CC333EE", type: VehicleType.miniBus, checkInTime: Date(), discountCard: nil)
+let vehicle12 = Vehicle(plate: "DD444GG", type: VehicleType.bus, checkInTime: Date(), discountCard: "DISCOUNT_CARD_006")
+let vehicle13 = Vehicle(plate: "AA111DD", type: VehicleType.car, checkInTime: Date(), discountCard: "DISCOUNT_CARD_007")
+let vehicle14 = Vehicle(plate: "B222BCB", type: VehicleType.moto, checkInTime: Date(), discountCard: nil)
+let vehicle15 = Vehicle(plate: "CC333XC", type: VehicleType.miniBus, checkInTime: Date(), discountCard: nil)
+let vehicle16 = Vehicle(plate: "DD444WD", type: VehicleType.bus, checkInTime: Date(), discountCard: "DISCOUNT_CARD_008")
+let vehicle17 = Vehicle(plate: "AA111HB", type: VehicleType.car, checkInTime: Date(), discountCard: "DISCOUNT_CARD_009")
+let vehicle18 = Vehicle(plate: "B222CTC", type: VehicleType.moto, checkInTime: Date(), discountCard: nil)
+let vehicle19 = Vehicle(plate: "CC333KD", type: VehicleType.miniBus, checkInTime: Date(), discountCard: nil)
+let vehicle20 = Vehicle(plate: "DD444QE", type: VehicleType.bus, checkInTime: Date(), discountCard: "DISCOUNT_CARD_010")
+let vehicle21 = Vehicle(plate: "AA111AC", type: VehicleType.car, checkInTime: Date(), discountCard: nil)
+let vehicle22 = Vehicle(plate: "B222DMD", type: VehicleType.moto, checkInTime: Date(), discountCard: nil)
+let vehicle23 = Vehicle(plate: "CC333NE", type: VehicleType.miniBus, checkInTime: Date(), discountCard: nil)
+let vehicle24 = Vehicle(plate: "DD444RG", type: VehicleType.bus, checkInTime: Date(), discountCard: "DISCOUNT_CARD_011")
+let vehicle25 = Vehicle(plate: "AA111YD", type: VehicleType.car, checkInTime: Date(), discountCard: nil)
+
+let vehicleArray: [Vehicle] = [vehicle1, vehicle2, vehicle3, vehicle4, vehicle5,
+                               vehicle6, vehicle7, vehicle8, vehicle9, vehicle10,
+                               vehicle11, vehicle12, vehicle13, vehicle14, vehicle15,
+                               vehicle16, vehicle17, vehicle18, vehicle19, vehicle20,
+                               vehicle21, vehicle22, vehicle23, vehicle24, vehicle25]
+
+for v in vehicleArray {
+    alkeParking.checkInVehicle(v) { check in
+        check ? print("Todo bien mono, le queda bien cuidado :)") : print("No mono, acá no fue. Revise a la vuelta")
     }
-})
+}
+
+alkeParking.checkOutVehicle(plate: "AA111AA") { rate in
+    print(rate)
+} onError: { error in
+    print(error)
+}
+
+
+
+//alkeParking.vehicles.forEach({ vehicle in
+//    DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+//        print(vehicle.parkedTime)
+//    }
+//})
 
 
